b34034dcaf476b9ddba0e370b496329d45b39299
Ryan Wang <ryan@weplaydots.com>
2022-12-05 12:57:28 -0500

    mirror

---
+++ src/shared/mirror.js
@@ -0,0 +1,21 @@
+class Mirror 
+{
+    constructor(x1, y1, x2, y2) 
+    {
+      // 2 points would form a line, makes sense 
+      this.a = createVector(x1, y1);
+      this.b = createVector(x2, y2);
+    }
+  
+    show() 
+    {
+      // set color and weight 
+      stroke('rgb(44, 111, 239)');
+      strokeCap(SQUARE);
+      strokeWeight(12.0);
+
+      // draw it 
+      line(this.a.x, this.a.y, this.b.x, this.b.y);
+    }
+  }
+  
