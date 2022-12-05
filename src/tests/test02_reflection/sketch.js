58a2d16843a9baa8a5fb8b6bbc1a6584a0dc73dd
Ryan Wang <ryan@weplaydots.com>
2022-12-05 16:16:42 -0500

    testing reflection in progress

---
+++ src/tests/test02_reflection/sketch.js
@@ -0,0 +1,33 @@
+let mirrors = [];
+let ray;
+let rayEmitter;
+
+function setup() 
+{
+  createCanvas(400, 400);
+
+  let x1 = random(width);
+  let x2 = random(width);
+  let y1 = random(height);
+  let y2 = random(height);
+  mirrors[0] = new Mirror(x1, y1, x2, y2);
+  rayEmitter = new RayEmitter();
+}
+
+function draw() {
+  clear();
+  background(220);
+
+  for (let mirror of mirrors) {
+    mirror.show();
+  }
+
+  rayEmitter.updatePos(mouseX, mouseY);
+  
+  
+  
+  rayEmitter.look(mirrors);
+  rayEmitter.show();
+}
+
+
