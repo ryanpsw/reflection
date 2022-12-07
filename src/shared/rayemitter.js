36de6039d816165c71eafddac180d91c6bbf3062
Ryan Wang <ryan@weplaydots.com>
2022-12-06 23:18:54 -0500

    clean up test code no longer in use

--- src/shared/rayemitter.js
+++ src/shared/rayemitter.js
@@ -4,26 +4,16 @@ class RayEmitter
     {
       this.pos = pos;
       this.ray = new Ray(this.pos, normalizedDirection);
-      // this.step = 0;
     }
   
     updatePos(x, y) 
     {
-      // just a little fun!
-      /*
-      this.step += 0.01;
-      this.ray.dir.x = 1 * cos(this.step);
-      this.ray.dir.y = 1 * sin(this.step);
-      this.ray.dir.normalize();
-      */
-
       this.pos.set(x, y);
     }
   
     look(obstructionsArray) 
     {
       this.resetAllWallsAndBoxes(obstructionsArray);
-    
       this.ray.cast(obstructionsArray);
     }
   
